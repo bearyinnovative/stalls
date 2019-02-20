c95c09e3b7a3fa2993abd8f4dc812990f924a2fb
shonenada <shonenada@gmail.com>
2019-02-20 23:17:39 +0800

    chore: Setup logging

--- deploy/gunicorn_conf.py
+++ deploy/gunicorn_conf.py
@@ -3,4 +3,9 @@ from envcfg.raw import stalls as config
 bind = "{}:{}".format(config.HOST, config.PORT)
 workers = int(config.GUNICORN_WORKERS)
 
+errorlog = '-'
+accesslog = '-'
+loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
 del config
